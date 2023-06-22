205432d7dd8bec7e62dc58aca3b4259248d1bde8
lordish <gitonga403@gmail.com>
2023-06-22 08:56:11 +0300

    final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def index
+    post = Post.all
+    render json: post
+  end
+
   def show
     post = Post.find(params[:id])
     
@@ -9,12 +17,20 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
+
+  def render_not_found_response
+    render json: {error: 'Not Found'}, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {error: invalid.record.errors }, status: :unprocessable_entity
+  end
 
   def post_params
     params.permit(:category, :content, :title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:index, :show, :update]
   resources :authors, only: [:show, :create]
 end
